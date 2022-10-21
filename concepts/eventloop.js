32da7fdd0faff068147d352b840a9c60136b1523
madhavanvv <madhavan1177@gmail.com>
2022-10-21 09:50:27 +0530

    concepts practice created

---
+++ concepts/eventloop.js
@@ -0,0 +1,14 @@
+//oct-14
+
+console.log("task-1");
+
+setTimeout(() => {
+  console.log("async---------------------task-2");
+}, 0);
+
+console.log("task-3");
+console.log("task-4");
+console.log("task-5");
+console.log("task-6");
+console.log("task-7");
+console.log("task-8");
