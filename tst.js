74699b4cc82abfbd4c5ac4db905f7ad0bc3c615c
madhavanvv <madhavan1177@gmail.com>
2022-10-22 16:31:04 +0530

    promise practice done

--- tst.js
+++ tst.js
@@ -1,16 +1,27 @@
-function two() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("22222222222");
-    }, 2000);
-  });
-}
+// function myOne() {
+//   console.log("11111111-waiting........");
+//   function mycall() {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         reject("some error occurred venu -504");
+//       }, 2000);
+//     });
+//   }
 
-async function myAll() {
-  console.log("11111111");
-  let aa = await two();
-  console.log(aa);
-  console.log("33333333");
-  console.log("44444444");
-}
-myAll();
+//   let aa = mycall();
+//   aa.then(() => {
+//     console.log(aa);
+//     console.log("33333333");
+//     console.log("44444444");
+//   }).catch((ee) => {
+//     console.log(ee);
+//     console.log("from catch block");
+//   });
+// }
+
+// myOne();
+
+let url = "https://jsonplaceholder.typicode.com/todos/1";
+let response = fetch(url);
+//console.log(typeof response);
+console.log(typeof response.then);
