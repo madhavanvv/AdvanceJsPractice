06ae94d22378c4aa142e04572ffea655c781191f
madhavanvv <madhavan1177@gmail.com>
2022-11-28 05:09:50 +0530

    dom pract

---
+++ dom/index.js
@@ -0,0 +1,5 @@
+const mylabel = document.createElement("li");
+mylabel.innerText = "valmikimadhavan";
+document.querySelector(".container").appendChild(mylabel);
+
+const mylabelOne = document.createElement("li");
--- tst.js
+++
@@ -1,3 +0,0 @@
-//reverse the array
-
-let aa = [4, 5, 6, 7, 8, 9];
